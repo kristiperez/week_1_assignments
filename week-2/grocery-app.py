ae499a504d5fdb056c03f1f20f6ebf0ea87b2068
Kristi Perez <kristi.perezbedford@gmail.com>
2019-07-01 16:02:32 -0500

    week 2 assignments added

---
+++ week-2/grocery-app.py
@@ -0,0 +1,67 @@
+
+shopping_lists = []
+
+
+class ShoppingList:
+    def __init__(self, title, address):
+        self.title = title
+        self.address = address
+        self.items = [] 
+        
+
+class GroceryItem:
+    def __init__(self, title, price, quantity):
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+
+
+user_input = ""
+
+while user_input != "q":
+
+    if user_input == "1":
+        for index in range(0,len(shopping_lists)):
+            shopping_list = shopping_lists[index]
+            print(f"{index+1} - {shopping_list.title}")
+            for index in range(0,len(shopping_list.items)):
+                item_list = shopping_list.items[index]
+                print(item_list.title)
+
+            
+    elif user_input == "2":
+
+        title = input("Enter a title for your shopping list: ")
+        address = input("Enter an address for shopping list: ")
+
+        shopping_list = ShoppingList(title, address)
+
+        shopping_lists.append(shopping_list)
+        
+        for index in range(0,len(shopping_lists)):
+            shopping_list = shopping_lists[index]
+            print(f"{index+1} - {shopping_list.title}")
+
+    elif user_input == "3":
+
+        shopping_list_number = int(input("Enter the shopping list number to add items to: "))
+        title = input("Enter a grocery item: ")
+        price = input("Enter a price for the grocery item: ")
+        quantity = input("Enter a quantity for the grocery item: ")
+
+        grocery_item = GroceryItem(title,price,quantity)
+
+        shopping_list_to_add_number_to = shopping_lists[shopping_list_number -1]
+
+        shopping_list_to_add_number_to.items.append(grocery_item)
+
+        for index in range(0,len(shopping_lists)):
+            shopping_list = shopping_lists[index]
+            print(f"{shopping_list.title}")
+            for index in range(0,len(shopping_list.items)):
+                item_list = shopping_list.items[index]
+                print(item_list.title)
+                #print(shopping_list.items[index].title) ----> could do this instead of the 2 lines above
+
+        
+    user_input = input("Enter 2 to input a shopping list, and 3 to enter a grocery item. Otherwise press q to quit or 1 to display your shopping lists: ")
